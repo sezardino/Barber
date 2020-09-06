acb371a6940bb361f2a71409a70d7d61336d272a
Edward <e.arekhva@gmail.com>
2020-09-06 10:00:49 +0200

    refactoring

--- js/main.js
+++ js/main.js
@@ -3,10 +3,24 @@ import Slider from './components/slider';
 import Form from './components/form';
 
 document.addEventListener('DOMContentLoaded', () => {
-  new Menu().init();
-  new Form().init();
+	new Menu().init();
+	new Form().init();
 
-  if(document.documentElement.clientWidth < 1200) {
-    new Slider('.slider', '.slide', '.dot', '.slider__title-p').init();
-  }
+	let slider = new Slider('.slider', '.slide', '.dot', '.slider__title-p');
+
+	if (document.documentElement.clientWidth < 1200) {
+		slider.init();
+	}
+
+	window.addEventListener('resize', () => {
+		if (document.documentElement.clientWidth < 1200) {
+			slider = new Slider('.slider', '.slide', '.dot', '.slider__title-p');
+			slider.init();
+		} else if (document.documentElement.clientWidth > 1200) {
+			slider = null;
+		}
+	});
+	// if (document.documentElement.clientWidth < 1200) {
+	// 	new Slider('.slider', '.slide', '.dot', '.slider__title-p').init();
+	// }
 });
