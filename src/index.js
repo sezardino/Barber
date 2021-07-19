7daad301b96563de07ddc6c5a437486647682b9c
Edward <edward.arechwa@gogomedia.pl>
2021-07-19 10:26:28 +0200

    add html template

--- src/index.js
+++ src/index.js
@@ -1,19 +1,4 @@
-// Test import of a JavaScript module
-import { example } from '@/js/example'
 import './template.html'
-
-// Test import of an asset
-import webpackLogo from '@/images/webpack-logo.svg'
 
 // Test import of styles
 import '@/styles/index.scss'
-
-// Appending to the DOM
-const logo = document.createElement('img')
-logo.src = webpackLogo
-
-const heading = document.createElement('h1')
-heading.textContent = example()
-
-const app = document.querySelector('#root')
-app.append(logo, heading)
